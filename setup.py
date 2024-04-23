cadd9238ee5995600a2df34f7602aac7db3a5f5b
Ravikumar <vhravi13@gmail.com>
2024-04-23 10:39:07 +0530

    Experimenting the solution setup on ipynb file

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 #to install local pakcages in virutal environment
 
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author="Ravikumar",
+    author_email="rvhattikal92@gmail.com",
+    install_requires=["openai","langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages(),
+
+)
